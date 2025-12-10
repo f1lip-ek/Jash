9b22872282a0eec65dea1e523893df316abcc9cc
filip <filipzitek@seznam.cz>
2025-12-10 22:26:55 +0100

    improving command mkdir (create directory)

--- src/main/java/org/example/commands/MakeDir.java
+++ src/main/java/org/example/commands/MakeDir.java
@@ -2,27 +2,17 @@ package org.example.commands;
 
 import org.example.User;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 
 public class MakeDir extends Command{
 
     @Override
     public String execute(String input) {
         String cesta = User.path + input;
-        Path slozkaPath = Paths.get(cesta);
-        try {
-            if (!Files.exists(slozkaPath)) {
-                Files.createDirectory(slozkaPath);
-            } else {
-                throw new IllegalArgumentException("Directory with this name already exists");
-            }
-        } catch (IOException e) {
-            System.err.println("Error creating folder: " + e.getMessage());
+        if (new File(cesta).mkdir()){
+            return "Directory created";
         }
-        return "";
+        return "Cant create";
     }
 
     @Override
