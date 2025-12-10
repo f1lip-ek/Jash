e5639f29afcf5e247a664c7e85f39d7587abc141
filip <filipzitek@seznam.cz>
2025-12-10 22:20:42 +0100

    improving command ls (list)

--- src/main/java/org/example/commands/ViewDirs.java
+++ src/main/java/org/example/commands/ViewDirs.java
@@ -10,22 +10,20 @@ public class ViewDirs extends Command{
     @Override
     public String execute(String input) {
         File[] file = new File(User.path).listFiles();
-        String[] list = new File(User.path).list();
-
 
         if (file == null) {
             return "";
         }
 
-        String vsechnySlozky = "";
+        StringBuilder allThings = new StringBuilder();
         for (int i = 0; i < file.length; i++) {
             if (file[i].isDirectory()) {
-                vsechnySlozky += "\u001B[31m" + file[i].getName() + "\u001B[0m ";
+                allThings.append(/*RED*/"\u001B[31m").append(file[i].getName()).append("\u001B[0m ");
             }else {
-                vsechnySlozky += "\u001B[32m" + file[i].getName() + "\u001B[0m ";
+                allThings.append(/*GREEN*/"\u001B[32m").append(file[i].getName()).append("\u001B[0m ");
             }
         }
-        return vsechnySlozky;
+        return allThings.toString();
     }
 
     @Override
